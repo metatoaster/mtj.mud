00125ca149e5cd8d0b8c34ba213043556837003b
Tommy Yu <at@metatoaster.com>
2007-12-13 20:52:53 +1300

    Got a shell somewhat working. Server sort of still works but not properly.

--- mud.py
+++ mud.py
@@ -7,11 +7,16 @@ import socket
 import SocketServer
 import logging
 import traceback
+import threading
+
+# workaround for locked sockets
+socket.setdefaulttimeout(0.5)
 
 logging.basicConfig(level=logging.DEBUG)
 
 HOST = ''
 PORT = 50000
+MAX_DATA_LEN = 512
 MAX_CMD_LEN = 1024
 MAX_BAD = 10
 CMD_TERM = ['\r', '\n']
@@ -31,12 +36,19 @@ valid_cmd = {
 }
 
 
-class MudThread:
+class MudDriver:
+    """The mud driver.
+    
+    This is where main events should execute in.
+    """
+
+
+class MudConnThread:
     """Mix-in class to handle each request in a new thread."""
 
     # Decides how threads will act upon termination of the
     # main process
-    daemon_threads = False
+    daemon_threads = True
 
     def process_request_thread(self, request, client_address):
         """Same as in BaseServer but as a thread.
@@ -53,7 +65,6 @@ class MudThread:
 
     def process_request(self, request, client_address):
         """Start a new thread to process the request."""
-        import threading
         t = threading.Thread(target = self.process_request_thread,
                              args = (request, client_address))
         if self.daemon_threads:
@@ -61,7 +72,7 @@ class MudThread:
         t.start()
 
 
-class ThreadingMudServer(MudThread, SocketServer.TCPServer):
+class ThreadingMudServer(MudConnThread, SocketServer.TCPServer):
     """Standard ThreadingTCPServer, extended to allow customization.
     """
     allow_reuse_address = True
@@ -192,17 +203,14 @@ class Soul():
         validChar = lambda x: 32 <= ord(x) <= 126
         data = ''
         rawq = self.rawq
-        while self.online:
-            data = self.request.recv(32)
+        while self.online and CHAR_TERM not in data:
+            data = self.request.recv(MAX_DATA_LEN)
             logging.log(0, 'received data (%02d|%s)' % 
                     (len(data), data.__repr__()))
             if not data:
                 self.online = False
             if data: # and validChar(data):
                 rawq.append(data)
-            if CHAR_TERM in data:
-                # XXX I am tired
-                break
 
         # do error validation here.
         # else:
@@ -291,6 +299,9 @@ class Soul():
             self.loop()
         except:
             # wow, something messed up bad.
+            # currently the login process is _outside_ the exception
+            # block, so if login craps out this will definitely be
+            # thrown.
             logging.warning('%s exception has leaked out of loop!' % str(self))
             logging.warning(traceback.format_exc())
             self.send('A critical error has occured!')
@@ -331,34 +342,115 @@ class Soul():
         self.send('Goodbye %s, see you soon.' % str(self.body))
 
 
+class MudServer():
 
-def start():
-    server = None
-    try:
-        print "Starting server...",
-        server = ThreadingMudServer((HOST, PORT), MudRequestHandler)
-        print "done."
-    except socket.error:
-        print "fail!"
-        raise
+    def __init__(self):
+        """Initalizes the server, set constants from config file, etc.
+        
+        Does nothing for now.
+        """
+        self.running = None
+        self.server = None
+        self.souls = []
+        self.t = None
 
-    # XXX - split this part off?
-    try:
-        while 1:
-            server.handle_request()
-    except KeyboardInterrupt:
-        print "Got Keyboard Interrupt."
-    finally:
-        if server:
+    def _begin(self):
+        try:
+            logging.info('Starting server...')
+            self.server = ThreadingMudServer((HOST, PORT), MudRequestHandler)
+            self.running = True
+            logging.info('Started server %s', self)
+        except socket.error:
+            logging.warn('Failed to start server %s', self)
+            raise
+
+    def _run(self):
+        while self.running:
+            self.server.handle_request()
+
+    def _end(self):
+        if self.server and self.running:
             try:
-                print "Server shutting down."
-                for soul in souls:
+                logging.info('Shutting down server %s.', self)
+                # FIXME - deal with threads?!
+                for soul in self.souls:
                     soul.send('Server shutting down.')
             finally:
-                server.server_close()
+                self.server.server_close()
+                self.running = False
         else:
             raise
-        sys.exit()
+        #sys.exit()
+
+    def _start(self):
+        # XXX - this is not a real start function, stuck in loop is
+        # still very stuck in loop.
+        self._begin()
+        try:
+            self._run()
+        finally:
+            self._end()
+
+    def start(self):
+        """Start a new thread to process the request."""
+        self.t = threading.Thread(target = self._start)
+        self.t.start()
+
+    def stop(self):
+        """Terminates server."""
+        # need to join self.t?
+        self._end()
+        self.t.join(5)
+        self.t.isAlive()
+
 
 if __name__ == '__main__':
-    start()
+    def start():
+        if not mudserv.running:
+            print 'Starting mudserv... ',
+            mudserv.start()
+            print 'done.'
+        else:
+            print 'mudserv already started.'
+
+    def stop():
+        if mudserv.running:
+            print 'Stopping mudserv... ',
+            mudserv.stop()
+            print 'done.'
+        else:
+            print 'mudserv not started.'
+
+    def quit():
+        # lolhack
+        raise EOFError
+
+    mudserv = MudServer()
+    active = True
+    server_cmd = {
+         'start': start,
+         'stop': stop,
+         'quit': quit,
+    }
+
+    print 'Starting mtmud interactive shell.'
+    while active:
+        try:
+            print('mudctrl>'),
+            s = raw_input()
+            if s in server_cmd:
+                server_cmd[s]()
+            else:
+                print 'Invalid Command.'
+        except EOFError:
+            active = False
+            print ''
+        except KeyboardInterrupt:
+            print('\nGot keyboard interrupt, terminating.')
+            active = False
+        except:
+            print traceback.format_exc()
+    # stop server if running
+    if mudserv.running:
+        mudserv.stop()
+
